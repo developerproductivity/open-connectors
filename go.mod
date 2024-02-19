6bfecb099355bd4da48e38b33c600a3225344208
Kartikey-star <kmamgain@redhat.com>
2024-02-19 12:54:47 +0530

    get project details
    Signed-off-by: Kartikey-star <kmamgain@redhat.com>

--- go.mod
+++ go.mod
@@ -3,9 +3,15 @@ module github.com/open-connectors/open-connectors
 go 1.21
 
 require (
-	github.com/google/go-github/v57 v57.0.0
 	github.com/gorilla/mux v1.8.1
 	github.com/urfave/negroni v1.0.0
+	golang.org/x/oauth2 v0.17.0
 )
 
-require github.com/google/go-querystring v1.1.0 // indirect
+require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
+	golang.org/x/net v0.21.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,19 +1,28 @@
 package main
 
 import (
+	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"net/http/httputil"
+	"os"
+	"strings"
+	"text/template"
 
-	gogi "github.com/google/go-github/v57/github"
 	"github.com/gorilla/mux"
 	"github.com/urfave/negroni"
+	"golang.org/x/oauth2"
 )
 
-func HandleWebhook(w http.ResponseWriter, r *http.Request) {
-	var req gogi.Issue
-	json.NewDecoder(r.Body).Decode(&req)
-	fmt.Println(req)
+func HandlePlanningData(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"test": "ok}`))
+}
+
+func HandleBuildData(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(`{"test": "ok}`))
@@ -29,14 +38,83 @@ func route() (n *negroni.Negroni, rt *mux.Router) {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/", Handler).Methods("GET")
-	router.HandleFunc("/", HandleWebhook).Methods("POST")
+	router.HandleFunc("/", HandlePlanningData).Methods("POST")
+	router.HandleFunc("/", HandleBuildData).Methods("POST")
 	n = negroni.New(negroni.NewRecovery(), negroni.NewLogger())
 	return n, router
 }
-func main() {
 
+func main() {
+	CallGithubGraphqlApi()
 	n, router := route()
 	n.UseHandler(router)
 	n.Run(":8080")
+}
+
+type graphQLRequest struct {
+	Query     string `json:"query"`
+	Variables string `json:"Variables"`
+}
+
+type Projectid struct {
+	Val string
+	Id  int
+}
+
+func CallGithubGraphqlApi() {
+	client := oauth2.NewClient(
+		context.TODO(),
+		oauth2.StaticTokenSource(
+			&oauth2.Token{AccessToken: os.Getenv("TOKEN")},
+		))
+
+	query := `{
+		node(id: "{{.Val}}") {
+		  ... on ProjectV2 {
+			fields(first: {{.Id}}) {
+			  nodes {
+				... on ProjectV2Field {
+				  id
+				  name
+				}
+				... on ProjectV2IterationField {
+				  id
+				  name
+				  configuration {
+					iterations {
+					  startDate
+					  id
+					}
+				  }
+				}
+				... on ProjectV2SingleSelectField {
+				  id
+				  name
+				  options {
+					id
+					name
+				  }
+				}
+			  }
+			}
+		  }
+		}
+	  }`
+
+	// parametes to the query
+	ids := Projectid{"PVT_kwDOCR9iiM4AbwsU", 20}
+	tmpl, err := template.New("new").Parse(query)
+	buf := &bytes.Buffer{}
+	err = tmpl.Execute(buf, ids)
+	if err != nil {
+		panic(err)
+	}
+	gqlMarshalled, err := json.Marshal(graphQLRequest{Query: buf.String()})
+	if err != nil {
+		panic(err)
+	}
+	resp, err := client.Post("https://api.github.com/graphql", "application/json", strings.NewReader(string(gqlMarshalled)))
+	b, _ := httputil.DumpResponse(resp, true)
+	fmt.Println(string(b))
 
 }
